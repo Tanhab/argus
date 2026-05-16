615dd8612bdcffb37e8fabfb7768f00ec04d0497
Tanhab Hossain <44816216+Tanhab@users.noreply.github.com>
2026-05-16 11:26:45 -0700

    chore(repo): install husky and commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,18 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'refactor', 'chore', 'docs', 'test', 'ci'],
+    ],
+    'scope-enum': [
+      2,
+      'always',
+      ['api', 'checker', 'db', 'logger', 'infra', 'docs', 'repo'],
+    ],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-max-length': [2, 'always', 72],
+    'subject-empty': [2, 'never'],
+  },
+};
